f018460353138df3e665e52fb957c72e153cd399
Guilherme Ferreira <gferreira525@gmail.com>
2021-06-02 00:37:12 -0300

    feat: implementation of sumBalances

--- src/Models/Balance.ts
+++ src/Models/Balance.ts
@@ -1,4 +1,4 @@
 export default interface Balance{
    address: string;
-   balance: number;
+   balance: bigint;
 }
--- src/Services/SumBalances.ts
+++ src/Services/SumBalances.ts
@@ -1,9 +1,17 @@
 import getBalances from './GetBalances';
+import Balance from '../Models/Balance';
 
 const sumBalances = async() => {
-   const balances = await getBalances();
-   console.log(balances);
-   return balances;
+   const balances: Balance[] = await getBalances();
+   return doSumBalances(balances);
+}
+
+const doSumBalances = async(balances: Balance[]) => {
+   const summed = balances.reduce((prev, current) => {
+      current.balance = prev.balance + current.balance;
+      return current;
+    });
+   return summed.balance;
 }
 
 export default sumBalances;
