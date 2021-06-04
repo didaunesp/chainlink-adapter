c85ae60649b3c4d78ff1c51191a7d13ec6caac6d
Guilherme Ferreira <gferreira525@gmail.com>
2021-06-03 22:56:48 -0300

    fix: change route from get to post

--- src/Controller/BaseController.ts
+++ src/Controller/BaseController.ts
@@ -3,11 +3,18 @@ import express from "express";
 const controller = async(req: express.Request, res: express.Response, service: CallableFunction) => {
    try{
       const result = await service();
-      return res.json(result);
+      return res.json({
+         jobRunId: req.body.id,
+         data: result,
+         result: JSON.stringify(result)
+      });
    }
    catch(error){
       console.log(error);
-      return res.status(500).json(error.message);
+      return res.status(500).json({
+         jobRunId: req.body.id,
+         status: "errored",
+         error: error.message});
    }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -5,11 +5,11 @@ import controller from './Controller/BaseController';
 
 const routes = express.Router();
 
-routes.get('/sum-balances', async (req: express.Request, res: express.Response) => {
+routes.post('/sum-balances', async (req: express.Request, res: express.Response) => {
    return controller(req, res, sumBalances);
 });
 
-routes.get('/highest-lowest-addresses', async (req: express.Request, res: express.Response) => {
+routes.post('/highest-lowest-addresses', async (req: express.Request, res: express.Response) => {
   return controller(req, res, highestLowest);
 });
 
